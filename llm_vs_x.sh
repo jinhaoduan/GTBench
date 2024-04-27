f5e436632ba1703986d6582984c2b33ecb7c23da
ZhuoxuanZhangHarry <zz3052@columbia.edu>
2024-04-27 16:35:21 +0000

    update

--- llm_vs_x.sh
+++ llm_vs_x.sh
@@ -1,20 +1,20 @@
 seed=0
 output_root="./experiments"
 exp_name='test'
-num_matches=50 # number of matches
-num_workers=20 # run 20 matches in parallel
+num_matches=1 # number of matches
+num_workers=1 # run 1 matches in parallel
 threshold_matches=100 # maximum number of matches, stop criteria for low completion rate, e.g., LLM agents always generate illegal actions.
 # suports all the games listed in ./gamingbench/configs/game_configs/*.yaml
-game_name='tictactoe'
+game_name='kuhn_poker'
 # supports all the llms defined in ./gamingbench/configs/model_configs/*.yaml
 model_config_root='gamingbench/configs/model_configs'
-llm_name='gpt-35-turbo-1106'
-opponent_llm_name='gpt-35-turbo-1106'
+llm_name='Bob'
+opponent_llm_name='Alice'
 # supports all the reasoning methods defined in ./gamingbench/agent_configs/*.yaml
 agent_config_root='gamingbench/configs/agent_configs'
 agent_name='prompt_agent'
-opponent_agent_name='cot_agent'
-declare -a api_keys=("<YOUR-OPENAIAPI-KEY>" "<YOUR_DEEPINFRA_KEY>")
+opponent_agent_name='prompt_agent'
+declare -a api_keys=("" "")  # Place your API keys here
 
 python3 -m gamingbench.main \
     --num-matches ${num_matches} \
