f2f21a05c9d2587416f4454445dd103390a5357e
Kevin Li <kgl2123@columbia.edu>
2024-05-04 01:45:27 -0400

    major updates to NRA_test.py

--- gamingbench/main.py
+++ gamingbench/main.py
@@ -7,11 +7,11 @@ from gamingbench.utils import utils
 from gamingbench.environments.base_env import BaseGameEnv
 import json
 
-# games = ['tictactoe', 'connect4', 'texasholdem', 'neuron_poker', 'backgammon', 'breakthrough',
-#          'first_sealed_auction', 'gin_rummy', 'liars_dice', 'negotiation', 'nim', 'pig', 'kuhn_poker',
-#          'prisoners_dilemma']
+games = ['tictactoe', 'connect4', 'texasholdem', 'neuron_poker', 'backgammon', 'breakthrough',
+         'first_sealed_auction', 'gin_rummy', 'liars_dice', 'negotiation', 'nim', 'pig', 'kuhn_poker',
+         'prisoners_dilemma', "crazy_eights", "dots_and_boxes"]
 
-games = ["crazy_eights"]
+# games = ["crazy_eights"]
 
 
 def get_args():
